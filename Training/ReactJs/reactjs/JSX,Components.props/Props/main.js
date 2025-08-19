8953fd4649c283580acbd986119d434859e157d3
NGUYENQUANGHUY1106 <nguyenquanghuy110605@gmail.com>
2025-08-19 23:16:23 +0700

    Dom Events

---
+++ Training/ReactJs/reactjs/JSX,Components.props/Props/main.js
@@ -0,0 +1,24 @@
+// props : đạo cụ 
+/**
+ * - React elements
+  -> Sử dụng props giống như với attribute của thẻ HTML
+  -> 2 props class, for => className, htmlFor
+  -> Phải tuân theo quy ước có sẵn
+
+- React components
+  -> Sử dụng props giống như đối số cho Component
+  -> Tự do đặt tên props
+        + Đặt theo camelCase
+        + Có thể bao gồm dấu gạch ngang
+
+- Chú ý:
+  -> Prop "key" là prop đặc biệt
+  -> Props cơ bản là đối số của Component
+  -> Props có thể là bất cứ kiểu dữ liệu gì
+  -> Sử dụng destructuring
+ */
+// React Element tương tự như những thẻ bên HTML
+// React Component  được gọi như là các funtion được tạo 
+
+//BTVN
+ 
