78e882c6b58df839dfd867bace95fbcba0641619
NGUYENQUANGHUY1106 <nguyenquanghuy110605@gmail.com>
2025-08-25 22:37:33 +0700

    useState

---
+++ Training/ReactJs/reactjs/Hooks/bai1trangthai/src/App.js
@@ -0,0 +1,27 @@
+
+import './App.css';
+import {useState} from 'react';
+function App() {
+  const [info, setInfo] = useState({
+    name: 'Nguyen Van A',
+    age: 18,
+    address: 'Ha Noi, VN'
+  })
+
+  const handleUpdate = () => {
+    setInfo(prev => ({
+      ...prev,
+      bio: 'Yêu màu hồng ^^'
+    }))
+  }
+
+  return (
+    <div className="App" style={{ padding: 20 }}>
+      <h1>{JSON.stringify(info)}</h1>
+      <button onClick={handleUpdate}>Update</button>
+    </div>
+  )
+}
+
+
+export default App;
